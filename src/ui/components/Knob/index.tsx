b8dc2822d12df72cdf5d20341cef73808b1092ea
Chris Milson <chris.w.milson@gmail.com>
2020-06-19 17:46:52 +0900

    Add Knob component

---
+++ src/ui/components/Knob/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './style.scss'
+import { ReactComponent as KnobSVG } from './face.svg'
+
+export interface KnobProps {
+  size?: number
+  step?: string | number
+  min?: number
+  max?: number
+  /** A number in the range min-max */
+  value: number
+  /**
+   * The angle (clockwise from twelve o'clock) that the knob will point to when
+   * value is 0.
+   */
+  startAngle?: number
+  /**
+   * The angle (clockwise from twelve o'clock) that the knob will point to when
+   * value is 1.
+   */
+  endAngle?: number
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Knob: React.FC<KnobProps> = ({
+  size = 100,
+  step = 1,
+  min = 0,
+  max = 1,
+  value,
+  startAngle = -150,
+  endAngle = 150,
+  onChange
+}) => {
+  const normalised = (value - min) / (max - min)
+
+  return (
+    <div className="Knob">
+      <KnobSVG
+        className="visual"
+        style={{
+          rotate: `${(endAngle - startAngle) * normalised + startAngle}deg`
+        }}
+        width={size}
+        height={size}
+      />
+      <input
+        className="input"
+        type="range"
+        {...{ value, min, max, step, onChange }}
+      />
+    </div>
+  )
+}
+
+export default Knob
