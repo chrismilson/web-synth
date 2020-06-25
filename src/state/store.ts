9395db279330730f4cef5bb5df3a228d10dcf2be
Chris Milson <chris.w.milson@gmail.com>
2020-06-25 15:00:49 +0900

    Add observe store method

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,26 @@
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { RootState } from './types/state'
 
 const store = createStore(reducer)
+
+export const observeStore = function<T>(
+  select: (state: RootState) => T,
+  onChange: (newValue: T) => void
+) {
+  let currentState: T
+
+  const handleChange = () => {
+    const newState = select(store.getState())
+    if (newState !== currentState) {
+      currentState = newState
+      onChange(currentState)
+    }
+  }
+
+  const unsubscribe = store.subscribe(handleChange)
+  handleChange()
+  return unsubscribe
+}
 
 export default store
