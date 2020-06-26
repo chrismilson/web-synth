8d310592da54f8b49a3abfeeedd1823be05f6dd6
Chris Milson <chris.w.milson@gmail.com>
2020-06-26 14:47:14 +0900

    Initialise engine on volume

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import VisibleTodoList from './containers/VisibleTodoList'
-import AddTodo from './containers/AddTodo'
-import Footer from './components/Footer'
+import store from '../state/store'
+import initEngine from '../engine'
+
+import VCO1 from './containers/VCO1'
+import VCO2 from './containers/VCO2'
+import Volume from './containers/Volume'
+import VCOMixer from './containers/VCOMixer'
+
+const unsubscribe = store.subscribe(() => {
+  const volume = store.getState().volume
+
+  if (volume !== 0) {
+    initEngine()
+    unsubscribe()
+  }
+})
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Shlappas Todo Template</h1>
-      <AddTodo />
-      <VisibleTodoList />
-      <Footer />
+      <VCO1 />
+      <VCO2 />
+      <VCOMixer />
+      <Volume />
     </div>
   )
 }
