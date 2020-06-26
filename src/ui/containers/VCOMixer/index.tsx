619448ad65c4ba39dc463d124d5cea9a55ef487c
Chris Milson <chris.w.milson@gmail.com>
2020-06-26 13:01:23 +0900

    implement ui for vco mixer

---
+++ src/ui/containers/VCOMixer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import VCO1Level from './vco1Level'
+import VCO2Level from './vco2Level'
+import './style.scss'
+
+const VCOMixer: React.FC = () => {
+  return (
+    <div className="VCOMixer">
+      VCO MIXER
+      <VCO1Level />
+      <VCO2Level />
+    </div>
+  )
+}
+
+export default VCOMixer
---
+++ src/ui/containers/VCOMixer/vco2Level.tsx
@@ -0,0 +1,39 @@
+import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux'
+import { RootState } from '../../../state/types/state'
+import Knob from '../../components/Knob'
+import { setVCO2Level } from '../../../state/actions'
+
+const mapStateToProps: MapStateToProps<
+  {
+    value: number
+    min: number
+    max: number
+    step: number
+    title: string
+    className: string
+    labels: React.ReactNode[]
+  },
+  {},
+  RootState
+> = state => ({
+  value: state.vcoMixer.vco2Level,
+  min: 0,
+  max: 1,
+  step: 0.001,
+  title: 'VCO 2 LEVEL',
+  className: 'vco2',
+  labels: [...Array(11)].map((_, i) => i)
+})
+
+const mapDispatchToProps: MapDispatchToProps<
+  {
+    handleChange: (value: number) => void
+  },
+  {}
+> = dispatch => ({
+  handleChange: value => {
+    dispatch(setVCO2Level(value))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Knob)
