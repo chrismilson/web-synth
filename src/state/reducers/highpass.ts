2ca3107770537e43e9bcded4e66820c2f9e864f4
Chris Milson <chris.w.milson@gmail.com>
2020-06-29 11:23:58 +0900

    Set initial highpass cutoff

--- src/state/reducers/highpass.ts
+++ src/state/reducers/highpass.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { ActionType } from '../types/actions'
 import { getSetter } from './common'
 
-const cutoff = getSetter(ActionType.SET_HIGHPASS_CUTOFF, 1)
+const cutoff = getSetter(ActionType.SET_HIGHPASS_CUTOFF, 0)
 
 const peak = getSetter(ActionType.SET_HIGHPASS_PEAK, 0)
 
