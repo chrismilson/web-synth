76b6715e8401c44bf4021327dc557a76985011b0
Chris Milson <chris.w.milson@gmail.com>
2020-06-29 14:40:37 +0900

    Reduce imports
    By importing the action type module as an object, we dont have to have a
    large list of the types at the beginning.

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,120 +1,117 @@
-import {
-  ActionType,
-  SetVolumeAction,
-  SetVCO1WaveShapeAction,
-  SetVCO1PulseWidthAction,
-  SetVCO1ScaleAction,
-  SetVCO2WaveShapeAction,
-  SetVCO2PitchAction,
-  SetVCO2ScaleAction,
-  SetVCO1LevelAction,
-  SetVCO2LevelAction,
-  SetPortamentoAction,
-  SetMasterTune,
-  SetFrequencyModulatorModulationGeneratorAction,
-  SetFrequencyModulatorEnvelopeGeneratorAction,
-  SetHighpassCutoffAction,
-  SetHighpassPeakAction,
-  SetLowpassCutoffAction,
-  SetLowpassPeakAction
-} from './types/actions'
+import * as actions from './types/actions'
 import { VCO1WaveShape, VCO2WaveShape } from './types/state'
 
-export const setVolume = (volume: number): SetVolumeAction => ({
+const ActionType = actions.ActionType
+
+export const setVolume = (volume: number): actions.SetVolumeAction => ({
   type: ActionType.SET_VOLUME,
   payload: volume
 })
 
 export const setVCO1WaveShape = (
   shape: VCO1WaveShape
-): SetVCO1WaveShapeAction => ({
+): actions.SetVCO1WaveShapeAction => ({
   type: ActionType.SET_VCO1_WAVE_SHAPE,
   payload: shape
 })
 
 export const setVCO1PulseWidth = (
   pulseWidth: number
-): SetVCO1PulseWidthAction => ({
+): actions.SetVCO1PulseWidthAction => ({
   type: ActionType.SET_VCO1_PULSE_WIDTH,
   payload: pulseWidth
 })
 
-export const setVCO1Scale = (scale: number): SetVCO1ScaleAction => ({
+export const setVCO1Scale = (scale: number): actions.SetVCO1ScaleAction => ({
   type: ActionType.SET_VCO1_SCALE,
   payload: scale
 })
 
 export const setVCO2WaveShape = (
   shape: VCO2WaveShape
-): SetVCO2WaveShapeAction => ({
+): actions.SetVCO2WaveShapeAction => ({
   type: ActionType.SET_VCO2_WAVE_SHAPE,
   payload: shape
 })
 
-export const setVCO2Pitch = (pitch: number): SetVCO2PitchAction => ({
+export const setVCO2Pitch = (pitch: number): actions.SetVCO2PitchAction => ({
   type: ActionType.SET_VCO2_PITCH,
   payload: pitch
 })
 
-export const setVCO2Scale = (scale: number): SetVCO2ScaleAction => ({
+export const setVCO2Scale = (scale: number): actions.SetVCO2ScaleAction => ({
   type: ActionType.SET_VCO2_SCALE,
   payload: scale
 })
 
-export const setVCO1Level = (level: number): SetVCO1LevelAction => ({
+export const setVCO1Level = (level: number): actions.SetVCO1LevelAction => ({
   type: ActionType.SET_VCO1_LEVEL,
   payload: level
 })
 
-export const setVCO2Level = (level: number): SetVCO2LevelAction => ({
+export const setVCO2Level = (level: number): actions.SetVCO2LevelAction => ({
   type: ActionType.SET_VCO2_LEVEL,
   payload: level
 })
 
-export const setPortamento = (port: number): SetPortamentoAction => ({
+export const setPortamento = (port: number): actions.SetPortamentoAction => ({
   type: ActionType.SET_PORTAMENTO,
   payload: port
 })
 
-export const setMasterTune = (tune: number): SetMasterTune => ({
+export const setMasterTune = (tune: number): actions.SetMasterTune => ({
   type: ActionType.SET_MASTER_TUNE,
   payload: tune
 })
 
 export const setFrequencyModulatorModulationGenerator = (
   level: number
-): SetFrequencyModulatorModulationGeneratorAction => ({
+): actions.SetFrequencyModulatorModulationGeneratorAction => ({
   type: ActionType.SET_FREQUENCY_MODULATOR_MODULATION_GENERATOR,
   payload: level
 })
 
 export const setFrequencyModulatorEnvelopeGenerator = (
   level: number
-): SetFrequencyModulatorEnvelopeGeneratorAction => ({
+): actions.SetFrequencyModulatorEnvelopeGeneratorAction => ({
   type: ActionType.SET_FREQUENCY_MODULATOR_ENVELOPE_GENERATOR,
   payload: level
 })
 
 export const setHighpassCutoff = (
   frequency: number
-): SetHighpassCutoffAction => ({
+): actions.SetHighpassCutoffAction => ({
   type: ActionType.SET_HIGHPASS_CUTOFF,
   payload: frequency
 })
 
-export const setHighpassPeak = (q: number): SetHighpassPeakAction => ({
+export const setHighpassPeak = (q: number): actions.SetHighpassPeakAction => ({
   type: ActionType.SET_HIGHPASS_PEAK,
   payload: q
 })
 
 export const setLowpassCutoff = (
   frequency: number
-): SetLowpassCutoffAction => ({
+): actions.SetLowpassCutoffAction => ({
   type: ActionType.SET_LOWPASS_CUTOFF,
   payload: frequency
 })
 
-export const setLowpassPeak = (q: number): SetLowpassPeakAction => ({
+export const setLowpassPeak = (q: number): actions.SetLowpassPeakAction => ({
   type: ActionType.SET_LOWPASS_PEAK,
   payload: q
+})
+
+export const setModulationGeneratorWaveForm = (
+  form: number
+): actions.SetModulationGeneratorWaveFormAction => ({
+  type: ActionType.SET_MODULATION_GENERATOR_WAVE_FORM,
+  payload: form
+})
+
+export const setModulationGeneratorFrequency = (
+  frequency: number
+): actions.SetModulationGeneratorFrequencyAction => ({
+  type: ActionType.SET_MODULATION_GENERATOR_FREQUENCY,
+  payload: frequency
 })
