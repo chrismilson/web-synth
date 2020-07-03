20a2858c34d9450273db8a999697d9d6301f9a0b
Chris Milson <chris.w.milson@gmail.com>
2020-07-03 11:57:33 +0900

    Implement keyboard node

---
+++ src/engine/custom-nodes/KeyboardNode.ts
@@ -0,0 +1,40 @@
+import TwelveTone from '../../tuning'
+import equalTemper from '../../tuning/references/equal-temper'
+
+/**
+ * The keyboard is an interesting problem, because it operates mainly on changes
+ * in state rather than current state. This is because when a key is pressed it
+ * should trigger something - the change from not pressed to pressed or vice
+ * versa is important, but the fact that a given key is down is not so
+ * important.
+ */
+export default class KeyboardNode {
+  scale: TwelveTone
+  /** The control frequency of the keyboard */
+  frequency: ConstantSourceNode
+  /** The trigger out of the keyboard: 1 when key is pressed and 0 otherwise. */
+  trigger: ConstantSourceNode
+
+  constructor(context: AudioContext) {
+    this.frequency = context.createConstantSource()
+    this.trigger = context.createConstantSource()
+
+    this.frequency.start()
+    this.trigger.start()
+
+    this.scale = new TwelveTone(440, equalTemper)
+
+    this.triggerNote = this.triggerNote.bind(this)
+    this.triggerNote(69, true)
+  }
+
+  triggerNote(note: number, on: boolean) {
+    console.log(note, on ? 'on' : 'off')
+    if (on) {
+      this.frequency.offset.value = this.scale.getNote(note - 69)
+      this.trigger.offset.value = 1
+    } else {
+      this.trigger.offset.value = 0
+    }
+  }
+}
