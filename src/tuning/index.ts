c09ccc54f737f0bfd3c2bdbb1c181e121cb3e3eb
Chris Milson <chris.w.milson@gmail.com>
2020-07-03 15:45:35 +0900

    Add changable key

--- src/tuning/index.ts
+++ src/tuning/index.ts
@@ -1,14 +1,36 @@
 import fiveLimit from './references/five-limit'
 
+export enum Key {
+  A = 0,
+  A_SHARP = 1,
+  B_FLAT = 1,
+  B = 2,
+  C = 3,
+  C_SHARP = 4,
+  D_FLAT = 4,
+  D = 5,
+  D_SHARP = 6,
+  E_FLAT = 6,
+  E = 7,
+  F = 8,
+  F_SHARP = 9,
+  G_FLAT = 9,
+  G = 10,
+  G_SHARP = 11,
+  A_FLAT = 11
+}
+
 export class TwelveTone {
   private baseOctave: number[]
+  private key: Key
 
-  constructor(fundamental: number, reference = fiveLimit) {
-    // this.baseOctave = fiveLimitReference.map(ratio => ratio * fundamental)
+  constructor(fundamental: number, reference = fiveLimit, key = Key.A) {
+    this.key = key
     this.baseOctave = reference.map(ratio => ratio * fundamental)
   }
 
   getNote(note: number) {
+    note -= this.key
     const key = ((note % 12) + 12) % 12
     const octave = Math.floor(note / 12)
     return this.baseOctave[key] * Math.pow(2, octave)
